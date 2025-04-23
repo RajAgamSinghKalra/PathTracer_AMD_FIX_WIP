e1b0aa5b95a26b016de9f9cb878b77a96e1afcf7
JNNGL <jnngles@gmail.com>
2025-04-23 22:55:07 +1100

    Remove unused code

--- shaders/lib/reflection/lambertian.glsl
+++
@@ -1,8 +0,0 @@
-#ifndef _LAMBERTIAN_GLSL
-#define _LAMBERTIAN_GLSL 1
-
-float bsdfLambertian(material mat, vec3 lightDirection, vec3 viewDirection) {
-    return mat.albedo / PI;
-}
-
-#endif // _LAMBERTIAN_GLSL
